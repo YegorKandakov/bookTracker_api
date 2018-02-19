73e73ebfb4dec8273de9f17e019c949b8c913710
Yegor Kandakov <yegorkandakov@gmail.com>
2018-02-19 21:48:28 +0200

    user email confirmation

---
+++ src/mailer.js
@@ -0,0 +1,30 @@
+import nodemailer from 'nodemailer';
+
+const from = '"BookTracker" <info@booktracker.com';
+
+function setup() {
+  return nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
+    }
+  });
+}
+
+export function sendConfirmationEmail(user) {
+  const transport = setup();
+  const email = {
+    from,
+    to: user.email,
+    subject: "Welcome to BookTracker",
+    text: `
+    Welcome to BookTracker. Please confirm your email.
+
+    ${user.generateConfirmationUrl()}
+    `
+  }
+
+  transport.sendMail(email);
+}
